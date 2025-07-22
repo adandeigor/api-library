28b969c20904c6eb5462192996f6178a2018d72b
adande igor <adandehustler@gmail.com>
2025-07-23 00:39:57 +0100

    database

--- app/generated/prisma/edge.js
+++ app/generated/prisma/edge.js
@@ -346,7 +346,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
--- app/generated/prisma/index.js
+++ app/generated/prisma/index.js
@@ -347,7 +347,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
