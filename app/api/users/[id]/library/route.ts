79eacc5013b39a70395d5b88e615a64faab582a5
adande igor <adandehustler@gmail.com>
2025-07-23 01:30:21 +0100

    library

--- app/api/users/[id]/library/route.ts
+++ app/api/users/[id]/library/route.ts
@@ -5,11 +5,14 @@ export async function GET(request: Request, context: { params: any }) {
   try {
     const headers = request.headers;
     const currentUserRole = headers.get("x-user-role") as UserRole;
+    const currentUserId = headers.get("x-user-id") 
 
     // 2. Vérification des permissions
-    const isAdmin = currentUserRole === UserRole.ADMIN || currentUserRole === UserRole.MANAGER;
-    if (!isAdmin ) {
-      return Response.json({ error: 'Permission refusée' }, { status: 403 });
+    const isAdmin =
+      currentUserRole === UserRole.ADMIN ||
+      currentUserRole === UserRole.MANAGER;
+    if (!isAdmin) {
+      return Response.json({ error: "Permission refusée" }, { status: 403 });
     }
     const { id } = context.params;
     if (isNaN(id)) {
@@ -19,31 +22,24 @@ export async function GET(request: Request, context: { params: any }) {
       );
     }
 
-    const logs = await prisma.actionLog.findMany({
-      where: { userId: id },
-    });
+    const library = prisma.library.findUnique({
+        where : {managerId : parseInt(currentUserId as string)}
+    })
 
-    if (!logs) {
-      return Response.json({
-        error: "Pas de logs pour ce manager",
-      });
-    }
-
-    return Response.json({
-        data : logs,
-        status : 200
+    Response.json({
+        data : library,
+        message : "Librairie récupérée avec succès",
+        status :  200
     })
   } catch (error) {
     if (error instanceof Error) {
         return Response.json({
-            message : "Erreur lors de la récupération des logs d'actions",
-            status : 500,
-            error : error.message
+            error : error.message,
+            status : 500
         })
     }
     return Response.json({
         error : error,
-        message : "Erreur de serveur",
         status : 500
     })
   }
